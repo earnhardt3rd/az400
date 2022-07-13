868db785c2316a094ea9a048f1b52ff96bc22791
Frank <frank.earnhardt@outlook.com>
2022-07-13 11:41:54 -0700

    2022_07_13-B

---
+++ lab08-00_buildWebApp_Resources.sh
@@ -0,0 +1,22 @@
+LOCATION='eastus'
+RG_NAME='az400m1501a-RG'
+ACR_NAME=az400m151acr$RANDOM$RANDOM
+APP_SVC_PLAN='az400m1501a-app-svc-plan'
+WEB_APP_NAME=az400m151web$RANDOM$RANDOM
+SQLDB_SRV_NAME=az400m15sqlsrv$RANDOM$RANDOM
+SQLDB_NAME='az400m15sqldb'
+
+//Create Resource Group
+az group create --name $RG_NAME --location $LOCATION
+// Create 
+az acr create --name $ACR_NAME --resource-group $RG_NAME --location $LOCATION --sku Standard --admin-enabled true
+az appservice plan create --name 'az400m1501a-app-svc-plan' --location $LOCATION --resource-group $RG_NAME --is-linux
+az webapp create --name $WEB_APP_NAME --resource-group $RG_NAME --plan $APP_SVC_PLAN --deployment-container-image-name elnably/dockerimagetest
+IMAGE_NAME=myhealth.web
+az webapp config container set --name $WEB_APP_NAME --resource-group $RG_NAME --docker-custom-image-name $IMAGE_NAME --docker-registry-server-url $ACR_NAME.azurecr.io/$IMAGE_NAME:latest --docker-registry-server-url https://$ACR_NAME.azurecr.io
+az sql server create --name $SQLDB_SRV_NAME --resource-group $RG_NAME --location $LOCATION --admin-user sqladmin --admin-password Pa55w.rd1234
+az sql db create --name $SQLDB_NAME --resource-group $RG_NAME --server $SQLDB_SRV_NAME --service-objective S0 --no-wait 
+az sql server firewall-rule create --name AllowAllAzure --resource-group $RG_NAME --server $SQLDB_SRV_NAME --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
+
+CONNECTION_STRING="Data Source=tcp:$SQLDB_SRV_NAME.database.windows.net,1433;Initial Catalog=$SQLDB_NAME;User Id=sqladmin;Password=Pa55w.rd1234;"
+az webapp config connection-string set --name $WEB_APP_NAME --resource-group $RG_NAME --connection-string-type SQLAzure --settings defaultConnection="$CONNECTION_STRING"
